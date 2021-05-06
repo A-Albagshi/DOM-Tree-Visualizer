84066d32f0e355000cc311150c7c8819a11ecb21
A-Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-05-06 04:16:02 +0300

    draw tree, hide and show 50%

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
-
-let html = document.querySelector('html')
-let body = document.querySelector('body')
+let html = document.querySelector('html');
+let body = document.querySelector('body');
 canvas.width = 2000;
 canvas.height = 2000;
-let cnvWidth = canvas.width
-let cnvHeight = canvas.height
+let cnvWidth = canvas.width;
+let cnvHeight = canvas.height;
 
 let radius = 40;
-let y = 50
+let y = 50;
 
 class Node {
     element;
@@ -27,18 +26,15 @@ class Node {
     }
 }
 
-let arr = new Array()
+let arr = new Array();
 let layer = 0;
-let path
-let bodyNode = new Node(document)
+let path;
+let bodyNode = new Node(document);
 
+nodesToArr(bodyNode);
+console.log(arr);
 
-nodesToArr(bodyNode)
-console.log(arr)
-
-drawTree()
-
-
+drawTree();
 
 function nodesToArr(node) {
     if (arr.length < layer + 1) {
@@ -47,7 +43,7 @@ function nodesToArr(node) {
     for (let i = 0; i < node.element.childNodes.length; i++) {
         if (
             node.element.childNodes[i].nodeType === 3 &&
-            node.element.childNodes[i].nodeValue.trim() === ""
+            node.element.childNodes[i].nodeValue.trim() === ''
         ) {
             continue;
         }
@@ -66,24 +62,29 @@ function nodesToArr(node) {
     }
 }
 
-
 function drawTree() {
     for (let i = 0; i < arr.length; i++) {
         y += 150;
         for (let j = 0; j < arr[i].length; j++) {
-            let x = cnvWidth * (j + 1) / (arr[i].length + 1)
+            let x = (cnvWidth * (j + 1)) / (arr[i].length + 1);
             const eleNode = arr[i][j];
+            arr[i][j].parent.children.push(arr[i][j])
             eleNode.x = x;
-            eleNode.y = y
+            eleNode.y = y;
+            if (!eleNode.parent.isActive) {
+                continue;
+            }
             context.beginPath();
             context.fillStyle = 'black';
-            context.font = "14px Arial";
+            context.font = '14px Arial';
             if (eleNode.element.nodeType === 3) {
                 context.fillRect(x - radius, y - radius / 2, radius * 3, radius);
                 context.fillStyle = 'White';
-                let text = (eleNode.element.textContent.length >= 10) ? eleNode.element.textContent.substring(0, 10) + "..." : eleNode.element.textContent;
+                let text =
+                    eleNode.element.textContent.length >= 10 ?
+                    eleNode.element.textContent.substring(0, 10) + '...' :
+                    eleNode.element.textContent;
                 context.fillText(text, eleNode.x, eleNode.y);
-
             } else {
                 context.arc(x, y, radius, radius, 0, 2 * Math.PI, false);
                 context.fill();
@@ -92,22 +93,48 @@ function drawTree() {
                 context.fillStyle = 'White';
                 context.textAlign = 'center';
                 context.fillText(nodeName, eleNode.x, eleNode.y);
-                context.fillStyle = (eleNode.isActive) ? 'green' : 'red';
+                context.fillStyle = eleNode.isActive ? 'green' : 'red';
                 context.rect(x - radius * 1.35, y, 15, 15);
-                context.fillText(eleNode.isActive ? "+" : "-", x - radius * 1.35 + 7, y + 10);
+                context.fillText(
+                    eleNode.isActive ? '+' : '-',
+                    x - radius * 1.35 + 7,
+                    y + 10
+                );
             }
-
-
 
             // Line Drawing
             context.moveTo(eleNode.parent.x, eleNode.parent.y);
             context.lineTo(eleNode.x, eleNode.y);
             context.stroke();
-
-
-
         }
     }
+}
 
+canvas.addEventListener('click', (e) => {
+    contains(e.offsetX, e.offsetY);
+});
 
+function contains(ClickX, ClickY) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            const eleNode = arr[i][j];
+
+            let marginX = eleNode.x - radius * 1.35 + 15;
+            let marginY = eleNode.y + 10;
+            // console.log(marginY)
+            if (ClickX <= marginX && ClickX >= marginX - 15 &&
+                ClickY - 5 <= marginY && ClickY >= marginY - 10
+            ) {
+                eleNode.isActive = !eleNode.isActive
+                context.clearRect(0, 0, cnvWidth, cnvWidth);
+                y = 50;
+                drawTree()
+
+            }
+            // console.log(ClickX)
+            // console.log(marginX = eleNode.x - radius * 1.35 + 15)
+            // console.log(ClickY)
+            // console.log(eleNode.y + 10)
+        }
+    }
 }
